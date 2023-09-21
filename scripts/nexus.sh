327336273e7903e4d19f54e37114998afdda2137
kelvin iriri <139765585+dizzy-101@users.noreply.github.com>
2023-09-21 19:58:16 +0100

    Update nexus.sh

--- scripts/nexus.sh
+++ scripts/nexus.sh
@@ -1,21 +1,21 @@
-#npm config list
-#npm config set registry http://54.197.105.125:8081/klik/repository/akugbe_nodejs/
+npm config list
+npm config set registry http://54.197.105.125:8081/klik/repository/akugbe_nodejs/
 
-# Provide your Nexus registry username and password
-#username=kelvin
-#password=jumped
+ Provide your Nexus registry username and password
+username=kelvin
+password=jumped
 
-# Run npm login command using expect
-#expect <<EOF
-#spawn npm login --registry
-#expect "Username: "
-#send "$username\r"
-#expect "Password: "
-#send "$password\r"
-#expect eof
-#EOF
+ Run npm login command using expect
+expect <<EOF
+spawn npm login --registry
+expect "Username: "
+send "$username\r"
+expect "Password: "
+send "$password\r"
+expect eof
+EOF
 
-#npm config set registry http://registry.npmjs.org/
-#npm config list
-#sleep 2
+npm config set registry http://registry.npmjs.org/
+npm config list
+sleep 2
 
