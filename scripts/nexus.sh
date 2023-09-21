baa08667fb956655a8d8a08243db954b3a2d71fe
kelvin iriri <139765585+dizzy-101@users.noreply.github.com>
2023-09-21 17:26:07 +0100

    Create nexus.sh

---
+++ scripts/nexus.sh
@@ -0,0 +1,20 @@
+npm config list
+npm config set registry http://54.197.105.125:8081/klik/repository/akugbe_nodejs/
+
+# Provide your Nexus registry username and password
+username=kelvin
+password=jumped
+
+# Run npm login command using expect
+expect <<EOF
+spawn npm login --registry
+expect "Username: "
+send "$username\r"
+expect "Password: "
+send "$password\r"
+expect eof
+EOF
+
+npm config set registry http://registry.npmjs.org/
+npm config list
+sleep 2
