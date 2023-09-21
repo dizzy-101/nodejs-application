6382a7ee35e61bdd7d70f21d445d8cf7aa9d6b3b
kelvin iriri <139765585+dizzy-101@users.noreply.github.com>
2023-09-21 17:31:27 +0100

    Create nexus.sh

---
+++ nexus.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/expect -f
+
+# Set the credentials
+set username "kelvin"
+set password "jumped"
+
+# Run the npm login command with expect
+spawn npm login --registry=http://54.197.105.125:8081/klik/repository/akugbe_nodejs/
+
+# Expect the username prompt and provide the username
+expect "Username:"
+send "$username\r"
+
+# Expect the password prompt and provide the password
+expect "Password:"
+send "$password\r"
+
+# Expect the email prompt and provide the email (if required)
+expect "Email: (this IS public)"
+send "\r"
+
+# Wait for the login process to complete
+expect eof
