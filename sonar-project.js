45f702856058a1a064ce45442eeeb8cb8b6a27cb
kelvin iriri <139765585+dizzy-101@users.noreply.github.com>
2023-09-21 16:26:47 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.160.115.108:9000/',
+  serverUrl: 'http://35.153.193.147:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
