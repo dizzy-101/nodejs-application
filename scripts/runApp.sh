4fa65f1b7923f8a2bc4ca034bd3d63680a369482
kelvin iriri <139765585+dizzy-101@users.noreply.github.com>
2023-09-21 17:38:21 +0100

    Update runApp.sh

--- scripts/runApp.sh
+++ scripts/runApp.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
-
-
-
 echo 'The following "npm" command runs your Node.js application'
 
+echo 'Trail to fix node app upload issue'
 set -x
 npm start & # for it to run in the background
+# node app.js &
+# npm install pm2 -g
+# pm2 start app.js
 sleep 1
 echo $! > .pidfile  # echo $! will show the scripts running in the background
 set +x
